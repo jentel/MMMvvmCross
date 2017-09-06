c3270362b182c068506a61ceb1b4d77bb469977f
JenniferT <jennifert@JenniferT-MacBookPro.local>
2017-09-06 15:33:31 -0500

    -got basic bottom navigation bar at the bottom of the first screen. Need to implement tabbing

---
+++ Collections.Droid/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Collections.Droid.Bootstrap
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
+    {
+    }
+}
---
+++ Collections.Droid/Views/FirstActivity.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Collections.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platform;
+
+namespace Collections.Droid.Views
+{
+	[Activity(
+        Label = "Main Activity",
+		Theme = "@style/AppTheme",
+		LaunchMode = LaunchMode.SingleTop,
+		Name = "collections.droid.views.FirstActivity"
+     )]
+    public class FirstActivity : MvxCachingFragmentCompatActivity<FirstViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.Page_First);
+        }
+    }
+}
